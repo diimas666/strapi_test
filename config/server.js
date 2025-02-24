e5fec009a684e308b4376cc158ed67d3a199f2a2
dima tikhtey <uu36548@gmail.com>
2025-02-24 13:48:36 +0200

    добавил порт

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', process.env.PORT || 1337), // ✅ Добавляем process.env.PORT
   app: {
     keys: env.array('APP_KEYS', ['mySuperSecretKeyA', 'mySuperSecretKeyB']),
   },
